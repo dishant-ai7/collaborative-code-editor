bde913239f2fdf1e5f1922cc543cd1e296010f0c
Dishant Toraskar <dishant.toraskar@coditas.com>
2024-07-13 01:30:56 +0530

    Initial setup with basic project files

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Started with collaborative-code-editor")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dishant-ai7/collaborative-code-editor
+
+go 1.22.5
